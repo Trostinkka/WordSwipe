f2d5e38ab789dd894ed93e7a9cf3e6bca8092282
Alex <romanov.allex055@gmail.com>
2026-06-28 11:07:43 +0300

    Initial Commit

---
+++ WordSwipe/WordSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordSwipeApp.swift
+//  WordSwipe
+//
+//  Created by Alex on 28.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct WordSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
